c5842b2ea686a1f17c524430fd08ffd7ed720d3d
Ryan Klein <ryanjklein34@gmail.com>
2022-07-07 20:26:42 -0400

    Final fix for windows compatibility

--- Source/SimulateGame.py
+++ Source/SimulateGame.py
@@ -142,7 +142,9 @@ def valid_words(word_list):
 def play_game(player1_bot, player2_bot, dictionary_path=None):
     global word_dictionary
     if not dictionary_path:
-        os.chdir('..')
+        current_folder = os.path.basename(os.getcwd())
+        if current_folder == 'Source':
+            os.chdir('..')
         dictionary_path = os.path.join(os.getcwd(), 'Input', 'scrabble_dictionary.txt')
     game = Game.ScrabbleGame(log_file_name="game_simulation.txt", sdata_name='game_simulation_data.txt',
                              sim_bag=True)
